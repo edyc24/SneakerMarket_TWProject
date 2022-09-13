90d0c8e2f5d77c63e7dd68f16229233d53825081
edyc24 <105642266+edyc24@users.noreply.github.com>
2022-09-13 20:30:31 +0300

    added clock on the second page

--- Noutati.js
+++ Noutati.js
@@ -1,3 +1,17 @@
+let Anunturi=[];
+
 window.onload=()=>{
+    clockElement=document.getElementById('ceas');
+    setInterval(()=>{
+        const d=new Date();
+        clockElement.innerHTML=`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    }, 1000);
+
+    numePereche=document.getElementById('nume');
+    numePereche.value=window.localStorage.getItem('nume');
+    pozaPereche=document.getElementById('poza-pereche');
+    descrierePereche=document.getElementById('descriere');
+    descrierePereche.value=window.localStorage.getItem('descriere');
+      
     
 }
